d152802efd4559c8fe6d550813f882eac3f56429
polurumanasa3 <manasap142@gmail.com>
2024-12-07 00:58:37 -0800

    Add files via upload

---
+++ m.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class m{
+    public static void main(String args[])
+    {
+    int arr[]={22,44,565,789,200};
+    System.out.println(Arrays.toString(arr));
+    System.out.println(arr[2]);
+    System.out.println(arr[3]);
+    arr[2]=456;
+    arr[3]=345;
+    System.out.println(Arrays.toString(arr));
+}
+}
